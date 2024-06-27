1b537deea24bc7219cf1430da19d3a8f2babfd6a
NicolasTeixeira2008 <nicolastxsilva2008@gmail.com>
2024-06-27 16:33:07 -0300

    atualização da landing page, envolvendo o css e java script com finalização do projeto

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//Constantes
+const fotoGrande = document.getElementById('fotoGrande')
+const desc = document.getElementById('desc')
+
+//Função para Trocar a foto
+function trocaimg(Value) {
+    fotoGrande.src = 'assets/' + Value
+}
